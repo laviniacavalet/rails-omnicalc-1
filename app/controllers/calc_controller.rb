5090f8362902565ea27b11d3286ff5f5b4452df1
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-22 23:12:20 +0000

    done with square root

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -14,6 +14,21 @@ end
 
     @the_result = @the_num ** 2
     render({:template => "calc_templates/square_results" })
+    
    end
+
+   def new_square_root
+    render({:template => "calc_templates/new_square_root" })
+   end
+
+  def square_root_results
+    @the_num = params.fetch("users_number").to_f
+ 
+  @the_result = Math.sqrt(@the_num) 
+  render({:template => "calc_templates/square_root_results" })
+  end
+
+
+
 
 end
