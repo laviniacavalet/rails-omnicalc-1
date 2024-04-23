af44f67421c5bf6479688020dc9845d78fa70e0e
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-23 00:12:59 +0000

    final version

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -28,7 +28,40 @@ end
   render({:template => "calc_templates/square_root_results" })
   end
 
+  def new_payment
+    render({:template => "calc_templates/new_payment" })
+  end 
+
+  def payment_results
+    @annual_rate = params.fetch("user_apr").to_f
+    @monthly_rate = (@annual_rate / 100) / 12
+    
+    @years = params.fetch("user_years").to_i
+    @number_of_payments = @years * 12
+  
+    @present_value = params.fetch("user_pv").to_f
+  
+    @monthly_payment = (@monthly_rate * @present_value) / (1 - (1 + @monthly_rate)**(-@number_of_payments))
+  
+ 
+    @formatted_annual_rate = (@monthly_rate*1200).to_fs(:percentage, precision: 4)
+    @formatted_present_value = @present_value.to_fs(:currency)
+    @formatted_monthly_payment = @monthly_payment.to_fs(:currency)
+    
+    render({ :template => "calc_templates/payment_results"})
+  end
 
 
+  def new_random
+    render({:template => "calc_templates/new_random" })
+  end
 
+  def random_results
+    @min_value = params.fetch("user_min").to_f
+  @max_value = params.fetch("user_max").to_f
+  
+  @random_number = rand(@min_value..@max_value)
+
+  render({:template => "calc_templates/random_results" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
  get("/square_root/new", {:controller => "calc", :action => "new_square_root"})
 
  get("/square_root/results", {:controller => "calc", :action => "square_root_results"})
+
+ get("/payment/new", {:controller => "calc", :action => "new_payment"})
+
+ get("/payment/results", {:controller => "calc", :action => "payment_results"})
+
+ get("/random/new", {:controller => "calc", :action => "new_random"})
+
+ get("/random/results", {:controller => "calc", :action => "random_results"})
 end
